bbf18270f961cdbd922dda2a0c48f9c397bb4185
Alex Chong <alex1107@gmail.com>
2018-11-29 21:12:06 -0500

    moved components into folder

--- src/components/Hot-or-cold.js
+++ src/components/Hot-or-cold.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "../logo.svg";
+import "./Hot-or-cold.css";
 
-class App extends Component {
+class Hotcold extends Component {
   render() {
     return (
       <div className="App">
@@ -25,4 +25,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Hotcold;
